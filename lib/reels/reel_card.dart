a636e4938fbb96e93ae8cd351f941348e731a55a
GokulGokzzz <gokulunniu88@gmail.com>
2025-02-05 16:05:10 +0530

    created the logo and name in reels screen

--- lib/reels/reel_card.dart
+++ lib/reels/reel_card.dart
@@ -9,6 +9,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:video_player/video_player.dart';
@@ -18,7 +19,8 @@ class ReelCard extends StatefulWidget {
   final bool play;
   final int index;
 
-  const ReelCard({Key? key, required this.index, this.play = false}) : super(key: key);
+  const ReelCard({Key? key, required this.index, this.play = false})
+      : super(key: key);
 
   @override
   State<ReelCard> createState() => _ReelCardState();
@@ -47,7 +49,9 @@ class _ReelCardState extends State<ReelCard> {
         Duration.zero,
         () {
           setState(() {
-            isLiked = (reelController.reels[widget.index].isLiked ?? 0) == 0 ? false : true;
+            isLiked = (reelController.reels[widget.index].isLiked ?? 0) == 0
+                ? false
+                : true;
           });
         },
       );
@@ -60,11 +64,15 @@ class _ReelCardState extends State<ReelCard> {
       isLiked = !isLiked;
     });
     if (isLiked) {
-      int newLike = (reelController.reels[widget.index].courseLikeCount ?? 0) + 1;
-      reelController.reels[widget.index] = reelController.reels[widget.index].copyWith(courseLikeCount: newLike);
+      int newLike =
+          (reelController.reels[widget.index].courseLikeCount ?? 0) + 1;
+      reelController.reels[widget.index] =
+          reelController.reels[widget.index].copyWith(courseLikeCount: newLike);
     } else {
-      int newLike = (reelController.reels[widget.index].courseLikeCount ?? 0) - 1;
-      reelController.reels[widget.index] = reelController.reels[widget.index].copyWith(courseLikeCount: newLike);
+      int newLike =
+          (reelController.reels[widget.index].courseLikeCount ?? 0) - 1;
+      reelController.reels[widget.index] =
+          reelController.reels[widget.index].copyWith(courseLikeCount: newLike);
     }
     final result = await reelController.likeDislike(courseId: id);
     // setState(() {
@@ -87,7 +95,8 @@ class _ReelCardState extends State<ReelCard> {
     setState(() {
       isCommentLoading = true;
     });
-    final result = await reelController.addComment(courseId: id, comment: commentController.text);
+    final result = await reelController.addComment(
+        courseId: id, comment: commentController.text);
 
     // if (result) {
     //   int newCount = (reelController.reels[widget.index].courseCommentCount ?? 0) + 1;
@@ -105,13 +114,16 @@ class _ReelCardState extends State<ReelCard> {
 
   Future<void> initializePlayer() async {
     log((widget.index.toString()), name: "index");
-    log(("${ApiConstants.publicBaseUrl}/${(reelController.reels[widget.index].courseReelVideo ?? "")}"), name: "initializePlayer");
-    log(("${(reelController.reels[widget.index].title ?? "")}"), name: "initializePlayer");
+    log(("${ApiConstants.publicBaseUrl}/${(reelController.reels[widget.index].courseReelVideo ?? "")}"),
+        name: "initializePlayer");
+    log(("${(reelController.reels[widget.index].title ?? "")}"),
+        name: "initializePlayer");
     try {
       setState(() {
         isLoading = true;
       });
-      final post = (reelController.reels[widget.index].courseReelVideo ?? "").isEmpty
+      final post = (reelController.reels[widget.index].courseReelVideo ?? "")
+              .isEmpty
           ? ("${ApiConstants.publicBaseUrl}/${(reelController.reels[widget.index].courseThumbnail ?? "")}")
           : ("${ApiConstants.publicBaseUrl}/${(reelController.reels[widget.index].courseReelVideo ?? "")}");
       _controller = VideoPlayerController.networkUrl(Uri.parse(post));
@@ -167,7 +179,8 @@ class _ReelCardState extends State<ReelCard> {
   @override
   void dispose() {
     super.dispose();
-    log(("${(reelController.reels[widget.index].title ?? "")}"), name: "dispose");
+    log(("${(reelController.reels[widget.index].title ?? "")}"),
+        name: "dispose");
 
     chewieController.dispose();
     _controller.dispose();
@@ -183,7 +196,8 @@ class _ReelCardState extends State<ReelCard> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Comments", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Text("Comments",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
               FutureBuilder(
                 future: reelController.getComments(courseId),
                 builder: (context, snapshot) {
@@ -230,7 +244,8 @@ class _ReelCardState extends State<ReelCard> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Likes", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Text("Likes",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
               FutureBuilder(
                 future: reelController.getLikes(courseId),
                 builder: (context, snapshot) {
@@ -269,119 +284,138 @@ class _ReelCardState extends State<ReelCard> {
   @override
   Widget build(BuildContext context) {
     final item = reelController.reels[widget.index];
+
     return SizedBox(
-      height: isShowComment ? MediaQuery.sizeOf(context).height * 0.7 : MediaQuery.sizeOf(context).height * 0.6,
-      child: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: MediaQuery.sizeOf(context).height * 0.5,
-                  child: isError
-                      ? CachedNetworkImage(
-                          imageUrl: "${ApiConstants.publicBaseUrl}/${reelController.reels[widget.index].courseThumbnail}",
-                          progressIndicatorBuilder: (context, url, progress) => SizedBox(
-                            height: 300,
-                            child: Center(
-                              child: CircularProgressIndicator(
-                                value: progress.progress,
+      height: isShowComment
+          ? MediaQuery.sizeOf(context).height * 0.7
+          : MediaQuery.sizeOf(context).height * 0.6,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.5,
+                child: isLoading
+                    ? Center(
+                        child: SpinKitSquareCircle(
+                          color: Colors.blue,
+                          size: 70.0,
+                        ),
+                      )
+                    : isError
+                        ? CachedNetworkImage(
+                            imageUrl:
+                                "${ApiConstants.publicBaseUrl}/${item.courseThumbnail}",
+                            progressIndicatorBuilder:
+                                (context, url, progress) => SizedBox(
+                              height: 300,
+                              child: Center(
+                                child: SpinKitFadingCircle(
+                                  color: Colors.blue,
+                                  size: 50.0,
+                                ),
                               ),
                             ),
+                            fit: BoxFit.cover,
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.broken_image),
+                          )
+                        : AspectRatio(
+                            aspectRatio: 16 / 9,
+                            child: Chewie(controller: chewieController),
                           ),
-                          fit: BoxFit.cover,
-                          errorWidget: (context, url, error) => Icon(Icons.broken_image),
-                        )
-                      : AspectRatio(
-                          aspectRatio: 16 / 9,
-                          child: Chewie(
-                            controller: chewieController,
-                          ),
-                        ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+              ),
+              Positioned(
+                top: 10,
+                left: 10,
+                right: 10,
+                child: Row(
                   children: [
-                    IconButton(
-                      icon: Icon(isLiked ? Icons.favorite : Icons.favorite_border, color: isLiked ? Colors.red : null),
-                      onPressed: () {
-                        _toggleLike(item.id!);
-                      },
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        _showLikeBottomSheet(item.id!);
-                      },
-                      child: Text("${item.courseLikeCount ?? 0}"),
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/app_logo.jpeg'),
                     ),
                     SizedBox(width: 10),
-                    IconButton(
-                      icon: Icon(
-                        Icons.comment,
+                    Expanded(
+                      child: Text(
+                        'Edxera',
+                        // item.courseName ?? '',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      onPressed: () {
-                        _showComment();
-                      },
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        _showCommentsBottomSheet(item.id!);
-                      },
-                      child: Text("${item.courseCommentCount ?? 0}"),
-                    ),
-                    Spacer(),
-                    IconButton(
-                      icon: Icon(Icons.share),
-                      onPressed: () {
-                        final post = (reelController.reels[widget.index].courseReelVideo ?? "").isEmpty
-                            ? ("${ApiConstants.publicBaseUrl}/${(reelController.reels[widget.index].courseThumbnail ?? "")}")
-                            : ("${ApiConstants.publicBaseUrl}/${(reelController.reels[widget.index].courseReelVideo ?? "")}");
-                        Share.shareUri(Uri.parse(post));
-                      },
                     ),
                   ],
                 ),
-                SizedBox(height: 20),
-                if (isShowComment)
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: commentController,
-                      focusNode: node,
-                      decoration: InputDecoration(
-                        hintText: 'Comment',
-                        hintStyle: TextStyle(fontFamily: 'Gilroy', fontSize: 15.sp, color: const Color(0XFF9B9B9B), fontWeight: FontWeight.w700),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(color: const Color(0XFFDEDEDE), width: 1.w),
-                          borderRadius: BorderRadius.circular(12),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              IconButton(
+                icon: Icon(isLiked ? Icons.favorite : Icons.favorite_border,
+                    color: isLiked ? Colors.red : null),
+                onPressed: () => _toggleLike(item.id!),
+              ),
+              GestureDetector(
+                onTap: () => _showLikeBottomSheet(item.id!),
+                child: Text("${item.courseLikeCount ?? 0}"),
+              ),
+              SizedBox(width: 10),
+              IconButton(
+                icon: Icon(Icons.comment),
+                onPressed: _showComment,
+              ),
+              GestureDetector(
+                onTap: () => _showCommentsBottomSheet(item.id!),
+                child: Text("${item.courseCommentCount ?? 0}"),
+              ),
+              Spacer(),
+              IconButton(
+                icon: Icon(Icons.share),
+                onPressed: () {
+                  final post = (item.courseReelVideo ?? "").isEmpty
+                      ? ("${ApiConstants.publicBaseUrl}/${(item.courseThumbnail ?? "")}")
+                      : ("${ApiConstants.publicBaseUrl}/${(item.courseReelVideo ?? "")}");
+                  Share.shareUri(Uri.parse(post));
+                },
+              ),
+            ],
+          ),
+          if (isShowComment)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: commentController,
+                focusNode: node,
+                decoration: InputDecoration(
+                  hintText: 'Comment',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xFFF5F5F5),
+                  suffixIcon: isCommentLoading
+                      ? SpinKitFadingCircle(
+                          color: Colors.blue,
+                          size: 20.0,
+                        )
+                      : IconButton(
+                          onPressed: () async {
+                            _addComment(item.id!);
+                          },
+                          icon: Icon(Icons.send),
                         ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: const Color(0XFF503494), width: 1.w),
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: const Color(0XFFDEDEDE), width: 1.w),
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        filled: true,
-                        fillColor: const Color(0xFFF5F5F5),
-                        contentPadding: EdgeInsets.only(left: 20.w, top: 20.h, bottom: 20.h),
-                        suffixIcon: isCommentLoading
-                            ? CupertinoActivityIndicator()
-                            : IconButton(
-                                onPressed: () async {
-                                  _addComment(item.id!);
-                                },
-                                icon: Icon(Icons.send),
-                              ),
-                      ),
-                    ),
-                  )
-              ],
+                ),
+              ),
             ),
+        ],
+      ),
     );
   }
 }
